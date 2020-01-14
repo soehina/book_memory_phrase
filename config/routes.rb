617c7d9669035268c787c357f2a6d53bb91e0649
soehina <kamuhina@gmail.com>
2020-01-14 22:12:51 +0900

    かんたんログイン機能、ログアウト機能の設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get '/'=>'home#index'
   get 'home/show'
 
   devise_scope :user do
     root "users/sessions#new"
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   devise_for :users, :controllers => {
     sessions: 'users/sessions'
   }
 
-  root to: "home#index"
+  # root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
