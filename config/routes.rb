db0ea276e3485f94ad89ac06cdfd8736a0589e2f
soehina <kamuhina@gmail.com>
2020-02-23 00:43:47 +0900

    レイアウト微調整　ユーザーページのバグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,14 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :home
-    devise_for :users, :controllers => {
+  devise_for :users, :controllers => {
       sessions: 'users/sessions'
-    }
+  }
   as :user do
     get 'home/show',:to => 'devise/registrations#edit',:as => :user_root
   end
+
+  root :to => 'home#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
