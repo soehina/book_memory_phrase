1add09140df2b9ba983c6794008e84b70e3dbcca
soehina <kamuhina@gmail.com>
2020-04-04 21:48:05 +0900

    seedデータ　修正

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,7 +1,7 @@
 User.create!([
-  {email: "kinako@example.com", encrypted_password: "$2a$11$BeubIifKZKPPdDzF1mY21.OhddpCZvJEphOZ9RTvuyRj5IyhXU9RW", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "kinako", image: "IMG_6077.jpg"},
-  {email: "hukushima@example.com", encrypted_password: "$2a$11$6hzOWgcXzn3n7cMfIEuAS.qDxDc6uIKn9Lv7ZuycB5d7eGvM2gVei", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "ふくしま", image: "IMG_7486.jpg"},
-  {email: "hinako@example.com", encrypted_password: "$2a$11$5JxFzlRniiPNzYF1Nv0rUe1NhEDYhye5.xxxLGWq1JhVABrElQMNC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "hinako", image: "download20191003183443.png"},
-  {email: "test@example.com", encrypted_password: "$2a$11$WwP86lx0GRIgmUxoAFK9/eG6n8ctVNhVXCRrpm1N.IUFlFWFg1iCq", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "testuser", image: nil},
-  {email: "hanabi@example.com", encrypted_password: "$2a$11$5/qdxrcMtdKbjF7EpAetueZCNU7kMJfjkqGiC1zdAyKA2q6my.yR.", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "はなび", image: "IMG_0431.jpg"}
+  {email: "kinako@example.com", password: "kinakokinako", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "kinako", image: "IMG_6077.jpg"},
+  {email: "hukushima@example.com", password: "hukushimahukushima", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "ふくしま", image: "IMG_7486.jpg"},
+  {email: "hinako@example.com", password: "hinakohinako", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "hinako", image: "download20191003183443.png"},
+  {email: "test@example.com", password: "testtest", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "testuser", image: nil},
+  {email: "hanabi@example.com", password: "hanabihanabi", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "はなび", image: "IMG_0431.jpg"}
 ])
