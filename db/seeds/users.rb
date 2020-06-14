3fd630a9302f3dafe02b06bcc7f1e4233738ce3a
soehina <kamuhina@gmail.com>
2020-06-14 13:50:29 +0900

    make it better

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,7 +1,46 @@
 User.create!([
-  {email: "kinako@example.com", password: "kinakokinako", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "kinako", image: "IMG_6077.jpg"},
-  {email: "hukushima@example.com", password: "hukushimahukushima", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "ふくしま", image: "IMG_7486.jpg"},
-  {email: "hinako@example.com", password: "hinakohinako", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "hinako", image: "download20191003183443.png"},
-  {email: "test@example.com", password: "testtest", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "testuser", image: nil},
-  {email: "hanabi@example.com", password: "hanabihanabi", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "はなび", image: "IMG_0431.jpg"}
+  {
+    email: "kinako@example.com",
+    password: "kinakokinako",
+    reset_password_token: nil,
+    reset_password_sent_at: nil,
+    remember_created_at: nil,
+    name: "kinako",
+    image: "IMG_6077.jpg"
+  },
+  {
+    email: "hukushima@example.com",
+    password: "hukushimahukushima",
+    reset_password_token: nil,
+    reset_password_sent_at: nil,
+    remember_created_at: nil,
+    name: "ふくしま",
+    image: "IMG_7486.jpg"
+  },
+  {
+    email: "hinako@example.com",
+    password: "hinakohinako",
+    reset_password_token: nil,
+    reset_password_sent_at: nil,
+    remember_created_at: nil,
+    name: "hinako",
+    image: "download20191003183443.png"
+  },
+  {
+    email: "test@example.com",
+    password: "testtest",
+    reset_password_token: nil,
+    reset_password_sent_at: nil,
+    remember_created_at: nil,
+    name: "testuser", image: nil
+  },
+  {
+    email: "hanabi@example.com",
+    password: "hanabihanabi",
+    reset_password_token: nil,
+    reset_password_sent_at: nil,
+    remember_created_at: nil,
+    name: "はなび",
+    image: "IMG_0431.jpg"
+  }
 ])
